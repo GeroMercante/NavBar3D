5fbfd006e976982c97164feca76658a84d74bc6c
Gerónimo Mercante <106752163+GeroMercante@users.noreply.github.com>
2022-07-26 11:59:06 -0300

    Add files via upload

---
+++ 3dNavBar/app.js
@@ -0,0 +1,8 @@
+//Menu "Hamburguesa" G-Mercante
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
